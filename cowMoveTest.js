5ad819f2d4979dac076a9f6dcc9670fa8c15d6cc
judah helland <judah@hellands.com>
2023-12-28 16:51:55 -0800

    pathing to water and grass works now for the most part, they still have some strange upwards and leftwards tendencies but they do at least path to SOME water and food correctly now it's just a matter of tweaking the balancing so they spread out properly overall today saw massive improvements, the code is getting a lot simpler and more robust

--- cowMoveTest.js
+++ cowMoveTest.js
@@ -11,7 +11,7 @@ let config = {
         default: 'arcade',
         arcade: {
             gravity: { y: 0 },
-            debug: true
+            debug: false
         }
     },
     fps: {
@@ -21,14 +21,11 @@ let config = {
 };
 
 let game = new Phaser.Game(config);
-let foodAvailable = 5000;
-let waterAvailable = 5000;
 let updateInterval = 25000 / 1;
-
 let cowGroup;
 let cows = 0;
 let gameOver = false;
-let initialCows = 30;
+let initialCows = 12;
 let mapGroup;
 function preload() {
     this.load.image('water', 'assets/water.png');
@@ -59,7 +56,7 @@ function create() {
             } else if (terrainType <= 8) {
                 name = 'thick_grass'
                 type = 'grass'
-                quantity = 10000;
+                quantity = 100;
                 color = 0x004d00;
             } else {
                 name = 'water'
@@ -72,10 +69,12 @@ function create() {
                 square.setName(name);
                 square.setData(type, quantity);
                 mapGroup.add(square);
+                console.log(square)
         }
     }
     for (let i = 1; i <= initialCows; i++) {
         let t = 0
+        ////console.log(mapGroup)
         let grassSquare = false;
         while (!grassSquare) {
             startingLocationX = Math.floor(Math.random()*1000);
@@ -86,10 +85,11 @@ function create() {
             }
             t++
             if (t > 300) {
-                console.log('something went wrong')
+                ////console.log('something went wrong')
                 return 
             }
         }
+        ////console.log('grass found!', startingLocationX, startingLocationY)
         createCow(`${cows++}`, startingLocationX, startingLocationY);
     }
 }
@@ -98,34 +98,18 @@ function createCow(id, x, y) {
     const cow = cowGroup.create(x, y, 'cow');
     cow.setCollideWorldBounds(true);
     cow.cowId = id;
-    cow.food = 20000;
+    cow.food = 8000;
     cow.age = 0;
-
-    cow.water = 3000;
-    cow.drinking = false;
-    cow.movingToWater = false;
-    cow.knownWater = [];
-    cow.waterHeading = [];
-    cow.thirst = 0;
-    cow.waterPicked = false;
-    cow.closestWater = [];
-    cow.isThirsty = false;
-
+    cow.knownFood = [];
     cow.movingToFood = false;
     cow.eating = false;
     cow.moving = false;
     cow.hunger = 0;
     cow.satisfied = false;
+    cow.children = 0;
     cow.foodHeading = [];
     cow.foodPicked = false;
-    cow.knownFood = [];
-
-    
-    cow.currentDistance = 0;
-    cow.children = 0;
-    cow.wandering = false;
-    cow.wanderDistance = 0;
-    cow.wanderDirection = 0;
+    cow.closestFood = [];
 }
 
 function killCow(cow, causeOfDeath) {
@@ -134,27 +118,24 @@ function killCow(cow, causeOfDeath) {
 }
 
 function moveCowLeft(cow) {
-    cow.setVelocityX(-50)
-    cow.water-=3
+    cow.setVelocityX(-100)
     cow.food -= 3
 }
 
 function moveCowRight(cow) {
-    cow.setVelocityX(50)
+    cow.setVelocityX(100)
     cow.food -= 3
-    cow.water-=3
 }
 
 function moveCowUp(cow) {
-    cow.setVelocityY(-50)
+    cow.setVelocityY(-100)
     cow.food -= 3
-    cow.water-=3
 }
 
 function moveCowDown(cow) {
-    cow.setVelocityY(50)
+    cow.setVelocityY(100)
+
     cow.food -= 3
-    cow.water-= 3
 }
 
 function findDistance(startX, startY, endX, endY) {
@@ -173,94 +154,6 @@ function findDistance(startX, startY, endX, endY) {
 
     return distanceY + distanceX
 }
-
-function cowLookForWater(cow) {
-    console.log('looking for water')
-    const searchRadius = 500;
-    for (let dx = -searchRadius; dx <= searchRadius; dx+=100) {
-        for (let dy = -searchRadius; dy <= searchRadius; dy+=100) {
-            const targetX = Math.floor(cow.x / 100) + dx;
-            const targetY = Math.floor(cow.y / 100) + dy;
-            let square = getSquareAt(targetX, targetY);
-            if (square && square.name === 'water') {
-                // cow.movingToWater = true;
-                const waterX = (targetX)
-                const waterY = (targetY)
-                cow.knownWater = [...cow.knownWater, [waterX, waterY]]
-                // if (cow.closestWater.length === 0) {
-                //     cow.closestWater[0] = waterX;
-                //     cow.closestWater[1] = waterY;
-                // } else {
-                //     let currentDistance = findDistance(cow.x, cow.y, waterX, waterY)
-                //     let activeDistance = findDistance(cow.x, cow.y, cow.closestWater[0], cow.closestWater[1])
-                //     if (currentDistance < activeDistance) {
-                //         cow.closestWater[0] = waterX;
-                //         cow.closestWater[1] = waterY;
-                //     }
-                // }
-            }
-        }
-    }
-    cowPickWaterSource(cow);
-}
-
-function cowPickWaterSource(cow) {
-    for (let i = 0; i < cow.knownWater.length; i++) {
-        if (cow.waterHeading.length === 0) {
-            cow.waterHeading = [cow.knownWater[i][0], cow.knownWater[i][1]]
-        }
-        let currentDistance = findDistance(cow.x, cow.y, cow.waterHeading[0], cow.waterHeading[1])
-        let newDistance = findDistance(cow.x, cow.y, cow.knownWater[i][0], cow.waterHeading[i][1])
-        if (newDistance < currentDistance) {
-            cow.waterHeading = [cow.knownWater[i][0], cow.knownWater[i][1]]
-        }
-    }
-    cow.movingToWater = true;
-    console.warn('water source picked for cow :', cow.cowId, cow.waterHeading)
-    cow.movingToWater = false;
-    cow.knownWater = [];
-    cow.waterPicked = false;
-    cow.closestWater = [];
-
-    cowMoveTowardWater(cow)
-}
-
-function cowMoveTowardWater(cow) {
-
-    if (cow.x < (cow.waterHeading[0])) {
-        moveCowRight(cow)
-    } else if (cow.x > (cow.waterHeading[0])) {
-        moveCowLeft(cow)
-    }  if (cow.y < (cow.waterHeading[1])) {
-        moveCowDown(cow)
-    }  else if (cow.y > (cow.waterHeading[1])) {
-        moveCowUp(cow)
-    }
-    const cowPosition = getSquareAt(cow.x, cow.y)
-    if (cowPosition.name === 'water') {
-        console.log(`cow ${cow.cowId} is at the water`)
-        cow.setVelocityX(0);
-        cow.setVelocityY(0);
-        cow.drinking = true;
-        cow.waterHeading = [];
-    } else {
-        cow.knownWater = [];
-        cow.drinking = false;
-        cow.waterHeading = []
-        cow.movingToWater = false;
-    }
-    
-    
-}
-
-
-
-
-
-
-
-
-
 
 
 
@@ -281,12 +174,12 @@ function cowLookForFood(cow) {
     cowPickFoodSource(cow);
 }
 
-
 function cowPickFoodSource(cow) {
     for (let i = 0; i < cow.knownFood.length; i++) {
         if (cow.foodHeading.length === 0) {
             cow.foodHeading = [cow.knownFood[i][0], cow.knownFood[i][1]];
         }
+        // console.log('IS THIS UNDEFINED: ',cow.knownFood[i][0])
         let currentDistance = findDistance(cow.x, cow.y, cow.foodHeading[0], cow.foodHeading[1])
         let newDistance = findDistance(cow.x, cow.y, cow.knownFood[i][0], cow.knownFood[i][1])
         if (newDistance < currentDistance) {
@@ -295,12 +188,33 @@ function cowPickFoodSource(cow) {
         }
     }
     cow.movingToFood = true;
+
+
+    // // cow.foodHeading = [cow.closestFood[0], cow.closestFood[1]]
+    // console.log('picked: ', cow.foodHeading);
+
+
+    // let foodArrSpot = Math.floor(Math.random() * 100);
+    
+    // // Create a copy of the knownFood array
+    // const knownFoodCopy = [...cow.knownFood];
+
+    // if (knownFoodCopy.length > 0) {
+    //     // Check if foodArrSpot is within the bounds of the array
+    //     if (foodArrSpot >= knownFoodCopy.length) {
+    //         foodArrSpot = knownFoodCopy.length - 1;
+    //     }
+
+    //     cow.foodHeading = [knownFoodCopy[foodArrSpot][0], knownFoodCopy[foodArrSpot][1]];
+    //     // console.log(knownFoodCopy[0]);
+    //     cow.movingToFood = true;
+    // } else {
+    //     killCow(cow, 'failed to see food');
+    // }
 }
 
-
-
-
 function cowMoveTowardFood(cow) {
+    // console.log('moving to food')
     if (cow.x < (cow.foodHeading[0])) {
         moveCowRight(cow)
     } if (cow.x > (cow.foodHeading[0])) {
@@ -340,12 +254,12 @@ function update() {
 
     if (cowGroup.countActive(true) === 0) {
         gameOver = true;
-        console.log("Game Over!");
+        ////console.log("Game Over!");
     }
 
     mapGroup.children.iterate(function(square) {
         if (square.name === 'water') {
-            square.data.list.water +=20;
+            square.data.list.water +=2;
         } else if (square.name === 'thick_grass') {
             square.data.list.grass +=2;
             if (square.data.list.grass < 500) {
@@ -364,7 +278,6 @@ function update() {
     cowGroup.children.iterate(function(cow) {
         if (cow !== undefined) {
             cow.food -= 4;
-            cow.water -= 2;
             cow.age++;
             if (cow.age > 50000) {
                 killCow(cow, 'being a boomer');
@@ -372,24 +285,7 @@ function update() {
             if (cow.food <= 0) {
                 killCow(cow, 'lack of food');
             }
-            if (cow.water <= 0) {
-                killCow(cow, 'thirst');
-            }
-            cow.thirst = Math.floor(cow.water / 1000);
             cow.hunger = Math.floor(cow.food / 1000);
-            if (cow.drinking) {
-                const cowPosition = getSquareAt(cow.x, cow.y)
-                if (cowPosition.data.list.water > 50) {
-                    cow.water += 50;
-                    cowPosition.data.list.water -= 50;
-                } else {
-                    cow.knownWater = [];
-                    cow.drinking = false;
-                    cow.waterHeading = []
-                    cow.movingToWater = false;
-                    cow.closestWater = [];
-                }
-            }
             if (cow.eating) {
                 const cowLocation = getSquareAt(cow.x, cow.y)
                 if (cowLocation.data.list.grass > 50) {
@@ -402,67 +298,58 @@ function update() {
                     cow.foodPicked = false;
                     cow.movingToFood = false;
                 }
+                // console.log('cow eating')
             }
-            if (cow.thirst <= 10) {
-                if (cow.isThirsty) {
-                    cowMoveTowardWater(cow)
-                } else {
-                    cow.isThirsty = true;
-                    cowLookForWater(cow);
+            if (cow.hunger <= 10 && !cow.eating) {
+                const cowLocation = getSquareAt(cow.x, cow.y);
+                if (cowLocation.name === 'thick_grass') {
+                    cow.eating = true;
+                    cow.movingToFood = false;
+                    cow.heading = []
+                    cow.knownFood = [];
+                    // console.log('cow is on food game will now end')
+                    cow.setVelocityX(0);
+                    cow.setVelocityY(0);
+                    return
                 }
-                
-               
-            }
-                if (cow.hunger <= 10 && !cow.eating && !cow.movingToWater && !cow.drinking) {
-                    const cowLocation = getSquareAt(cow.x, cow.y);
-                    if (cowLocation.name === 'thick_grass') {
-                        cow.eating = true;
-                        cow.movingToFood = false;
-                        cow.foodHeading = []
-                        cow.knownFood = [];
-                        // console.log('cow is on food game will now end')
-                        cow.setVelocityX(0);
-                        cow.setVelocityY(0);
-                        return
-                    }
-                    if (!cow.movingToFood && !cow.eating) {
-                            cowLookForFood(cow);
-                            // console.log('cow just looked for food')
-                            cowPickFoodSource(cow);
-                            // console.log('cow just picked a food source')
+                if (!cow.movingToFood) {
+                        cowLookForFood(cow);
+                        // console.log('cow just looked for food')
+                        cowPickFoodSource(cow);
+                        // console.log('cow just picked a food source')
 
 
-                    } else {
-                        cowMoveTowardFood(cow);
-                        // console.log('cow just moved towards food')
-                        //console.log(`cow ${cow.cowId} moving towards: `, cow.foodHeading)
-                        //console.log(cow)
-                    }
+                } else {
+                    cowMoveTowardFood(cow);
+                    // console.log('cow just moved towards food')
+                    //console.log(`cow ${cow.cowId} moving towards: `, cow.foodHeading)
+                    //console.log(cow)
+                }
             }
     
-                if (cow.drinking && cow.water >= 20000) {
-                    cow.isThirsty = false;
-                } else if (cow.eating && cow.food >= 30000) {
+             if (cow.eating && cow.food >= 30000) {
                     cow.eating = false;
                     cow.foodHeading = [];
-                }
-                if (cow.drinking && cow.water >= 30000) {
-                    cow.drinking = false;
+                    //console.log('cow is full')
                 }
     
-                if (!cow.drinking && !cow.eating && !cow.movingToFood && !cow.movingToWater && 10000 < cow.age && cow.age < 80000) {
+                if (!cow.eating && !cow.movingToFood && 10000 < cow.age && cow.age < 80000) {
                     const chanceToReproduce = Math.floor(Math.random() * 10000);
-                    if (chanceToReproduce < 3 && cow.children <= 2) {
+                    if (chanceToReproduce < 10 && cow.children <= 5) {
                         createCow(`${cows++}`, cow.x, cow.y);
-                        console.log(cowGroup.children.entries.length);
+                        //console.log(cowGroup.children.entries.length);
                     } else if (chanceToReproduce < 3) {
                         killCow(cow, 'overpopulation');
-                        console.log('WATER:', waterAvailable);
+                        //console.log('WATER:', waterAvailable);
                     }
                 }
+                cow.knownFood = []
             }
-        
+            // if (cow.eating) {
+            //     gameOver = true;
+            // }
     });
+
 }
 
 function getSquareAt(x, y) {
@@ -478,3 +365,5 @@ function getSquareAt(x, y) {
     // Return null if no square is found at the given coordinates
     return null;
 }
+
+//SUBTRACTED THE + 50'S FROM THE FOODX AND FOODY WHEN THEY ARE SET
--- mapTest.js
+++ mapTest.js
@@ -1,30 +1,3 @@
-const terrains = ['grass', 'grass', 'grass', 'grass', 'thick_grass', 'thick_grass', 'water'];
-
-// const waterCap = 150;
-const waterCap = 10;
-const thickGrassCap = 15;
-// const thickGrassCap = 150;
-let waterAdj = false;
-// let map = new Array(100);
-let map = new Array(10);
-let waterNum = 0;
-let thickGrassNum = 0;
-for (let i = 0; i < map.length; i++) {
-//   map[i] = new Array(100);
-  map[i] = new Array(10);
-}
-let chanceOfWater = 25;
-let bodiesOfWater = 0;
-let waterMap = [];
-let waterUp;
-let waterLeft;
-let waterLengthCount = 0;
-let waterWidthCount = 0;
-// const maxCows = 40;
-let waterAvailable = 10000;
-let foodAvailable = 18000;
-
-
 let config = {
     type: Phaser.AUTO,
     width: 1000,
@@ -63,12 +36,6 @@ function preload() {
 }
 
 function create() {
-    // for (let i = 0; i < map.length; i++) {
-    //     for (let j = 0; j < map[i].length; j++) {
-    //         let image = this.add.image(i * 100, j * 100, map[i][j]);
-    //         image.setOrigin(0, 0);
-    //     }
-    // }
     this.physics.world.gravity.y = 0;
     this.physics.world.setBounds(0, 0, 1000, 1000);
     cowGroup = this.physics.add.group();
@@ -78,46 +45,45 @@ function create() {
     for (let i = 0; i < 10; i++) {
         for (let j = 0; j < 10; j++) {
             let terrainType = Math.floor(Math.random()*10)
+            let name;
+            let type;
+            let quantity;
+            let color;
             if (terrainType <= 5) {
-                const square = this.add.rectangle(i * 100, j * 100, 100, 100, 0x339966);
-                square.setOrigin(0, 0);
-                square.setName('light_grass');
-                square.setData('grass', 100);
-                mapGroup.add(square);
-
+                name = 'light_grass'
+                type = 'grass'
+                quantity = 1000;
+                color = 0x339966;
             } else if (terrainType <= 8) {
-                const square = this.add.rectangle(i * 100, j * 100, 100, 100, 0x004d00);
-                square.setOrigin(0, 0);
-                square.setData('grass', 1000);
-                square.setName('thick_grass');
-                mapGroup.add(square);
-
+                name = 'thick_grass'
+                type = 'grass'
+                quantity = 10000;
+                color = 0x004d00;
             } else {
-                const square = this.add.rectangle(i * 100, j * 100, 100, 100, 0x6699ff);
-                square.setData('water', 1000);
-                square.setOrigin(0, 0);
-                square.setName('water');
-                mapGroup.add(square);
-                console.log(square)
-                
+                name = 'water'
+                type = 'water'
+                quantity = 10000;
+                color = 0x6699ff;
             }
+                const square = this.add.rectangle(i * 100, j * 100, 100, 100, color);
+                square.setOrigin(0, 0);
+                square.setName(name);
+                square.setData(type, quantity);
+                mapGroup.add(square);
         }
     }
     for (let i = 1; i <= initialCows; i++) {
-        // let startingLocationX = Math.floor(Math.random()*100);
-        // let startingLocationY = Math.floor(Math.random()*100);
         let t = 0
         console.log(mapGroup)
         let grassSquare = false;
         while (!grassSquare) {
-            startingLocationX = Math.floor(Math.random()*100);
-            startingLocationY = Math.floor(Math.random()*100);
-            const squareAtLocation = getSquareAt(startingLocationX*10, startingLocationY*10)
+            startingLocationX = Math.floor(Math.random()*1000);
+            startingLocationY = Math.floor(Math.random()*1000);
+            const squareAtLocation = getSquareAt(startingLocationX, startingLocationY)
             if (squareAtLocation.name === 'light_grass') {
                 grassSquare = true;
             }
             console.log('HERE: ', squareAtLocation.name)
-            // console.log(map[startingLocationX*10][startingLocationY*10], startingLocationY, startingLocationX)
             t++
             if (t > 300) {
                 console.log('something went wrong')
@@ -125,15 +91,12 @@ function create() {
             }
         }
         console.log('grass found!', startingLocationX, startingLocationY)
-        createCow(`${cows++}`, startingLocationX*10, startingLocationY*10);
+        createCow(`${cows++}`, startingLocationX, startingLocationY);
     }
 }
 
 function createCow(id, x, y) {
     const cow = cowGroup.create(x, y, 'cow');
-    console.log('cow created', cow.x, cow.y)
-    console.log(cow)
-    cow.tabIndex = 1;
     cow.setCollideWorldBounds(true);
     cow.cowId = id;
     cow.food = 8000;
@@ -175,11 +138,3 @@ function getSquareAt(x, y) {
     // Return null if no square is found at the given coordinates
     return null;
 }
-
-function getSquareInfo() {
-        for (let square in mapGroup) {
-        console.log(square)
-    }
-}
-
-getSquareInfo()
